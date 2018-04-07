d1bc311f253796e0acee9b912a0d455ac3f549c2
Jeff Taggart <jeff@jetaggart.com>
2018-04-07 15:43:56 -0600

    fetch first kitty id

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
+var web3js;
+
+console.log(window.web3);
+if (typeof window.web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(window.web3.currentProvider);
+} else {
+    console.error("failure");
+}
+
+console.log(web3js.eth.accounts[0]);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to hell</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = { address: '0x7f1382e17d7969ee5ac6a5107506cb6dc13bf9d8'};
+    }
+
+    submit = () => {
+        console.error(this.state.address);
+        fetch(`https://api.cryptokitties.co/kitties?owner_wallet_address=${this.state.address}`)
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.kitties.length < 1) {
+                    console.alert("no kitties, please make sure there's one kitty");
+                    return;
+                }
+                var kitty = json.kitties[0];
+                console.log(kitty.id);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    handleAddresss = (event) => {
+        this.setState({address: event.target.value});
+    }
+
+    render() {
+        return (
+                <div className="App">
+                <input id='address' placeholder='address' onChange={this.handleAddress} value={this.state.value}/>
+                <br/>
+                <input id='privateKey' placeholder='privateKey'/>
+                <br/>
+                <button id='submit' onClick={this.submit}>Submit</button>
+                </div>
+        );
+    }
+
 }
 
 export default App;
